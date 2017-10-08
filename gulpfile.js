3b8e08bd46f39e107f9e354975320e65946c47b1
Maks <ffwmakssherbyna@gmail.com>
2017-10-08 16:55:48 +0300

    init commit

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+    return gulp.src('dist/scss/**/*.scss')
+      .pipe(sass().on('error', sass.logError))
+      .pipe(gulp.dest('app/css/'));
+  });
+
+gulp.task('watch', function () {
+    gulp.watch('dist/scss/**/*.scss', ['sass']);
+});
